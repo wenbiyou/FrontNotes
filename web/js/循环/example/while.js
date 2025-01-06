a0c52a2856c2cd4de0ce1d1b3e35cbd43df51d07
ngz <wenbiyou@163.com>
2025-01-06 16:47:07 +0800

    fix: 更新while循环demo

--- web/js/循环/example/while.js
+++ web/js/循环/example/while.js
@@ -5,3 +5,21 @@ while (n < 3) {
 }
 
 console.log(n);
+
+// const iterator = document.createNodeIterator(
+//   document,
+//   NodeFilter.SHOW_COMMENT
+// );
+// let currentNode;
+// while ((currentNode = iterator.nextNode()) && currentNode) {
+//   console.log(currentNode.textContent.trim());
+// }
+
+// for循环对于while循环的变体
+// for (
+//   let currentNode = iterator.nextNode();
+//   currentNode;
+//   currentNode = iterator.nextNode()
+// ) {
+//   console.log(currentNode.textContent.trim());
+// }
